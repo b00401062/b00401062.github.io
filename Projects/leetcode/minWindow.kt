ed9c384d9ce91fb400713d0566a2cf1950a7f879
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-03-21 12:01:56 +0800

    Add files via upload

---
+++ Projects/leetcode/minWindow.kt
@@ -0,0 +1,63 @@
+package leetcode
+
+import java.util.HashMap
+
+private fun letterMap(t: String): MutableMap<Char, Int> {
+    val letterMap: MutableMap<Char, Int> = HashMap()
+    for (c in t.toCharArray()) {
+        val deficit = letterMap.getOrDefault(c, 0)
+        letterMap[c] = deficit + 1
+    }
+    return letterMap
+}
+
+fun minWindow(s: String, t: String): String {
+    val letterMap = letterMap(t)
+    var curWindow = Window.from(s, letterMap) ?: return ""
+    curWindow.refine(s, letterMap)
+    var minWindow = curWindow
+    for (end in curWindow.end until s.length) {
+        val endC = s[end]
+        if (!letterMap.containsKey(endC)) continue
+        letterMap[endC] = letterMap[endC]!! - 1
+        curWindow = Window(curWindow.start, end + 1)
+        curWindow.refine(s, letterMap)
+        minWindow = if (minWindow.compareTo(curWindow) == -1) minWindow else curWindow
+    }
+    return s.substring(minWindow.start, minWindow.end)
+}
+
+private class Window internal constructor(var start: Int, var end: Int) : Comparable<Window> {
+    override fun compareTo(that: Window): Int {
+        val thisLength = end - start
+        val thatLength = that.end - that.start
+        return if (thisLength > thatLength) 1 else if (thisLength == thatLength) 0 else -1
+    }
+
+    fun refine(s: String, letterMap: MutableMap<Char, Int>) {
+        while (true) {
+            val c = s[start]
+            val deficit = letterMap.getOrDefault(c, Int.MAX_VALUE)
+            if (deficit == 0) return
+            start++
+            if (deficit > 0) continue
+            letterMap[c] = deficit + 1
+        }
+    }
+
+    companion object {
+        fun from(s: String, letterMap: MutableMap<Char, Int>): Window? {
+            var start = -1
+            for (i in 0 until s.length) {
+                val c = s[i]
+                if (!letterMap.containsKey(c) && start == -1) continue
+                start = if (start == -1) i else start
+                if (!letterMap.containsKey(c)) continue
+                letterMap[c] = letterMap[c]!! - 1
+                if (letterMap.values.stream().anyMatch { x: Int -> x > 0 }) continue
+                return Window(start, i + 1)
+            }
+            return null
+        }
+    }
+}
---
+++ Projects/leetcode/mostCompetitive.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+import java.util.Stack
+
+fun mostCompetitive(nums: IntArray, k: Int): IntArray {
+    val stack = Stack<Int>()
+    for (i in nums.indices) {
+        val num = nums[i]
+        while (!stack.isEmpty() && stack.peek() > num && k - stack.size < nums.size - i) {
+            stack.pop()
+        }
+        if (stack.size < k) {
+            stack.push(num)
+        }
+    }
+    return stack.stream().mapToInt { obj: Int -> obj }.toArray()
+}
