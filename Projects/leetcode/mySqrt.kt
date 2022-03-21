e45566aa1a8c1500ea17de057dbbefe0b75918e3
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-03-21 11:34:36 +0800

    Add files via upload

---
+++ Projects/leetcode/mySqrt.kt
@@ -0,0 +1,16 @@
+package leetcode
+
+import leetcode.MySqrt
+
+internal object MySqrt {
+    private fun myRecursiveSqrt(guess: Long, x: Int): Long {
+        return if (guess * guess <= x && x < (guess + 1) * (guess + 1)) guess else myRecursiveSqrt(
+            (guess + x / guess) / 2,
+            x
+        )
+    }
+
+    fun mySqrt(x: Int): Int {
+        return if (x == 0) 0 else if (x == 1) 1 else myRecursiveSqrt((x / 2).toLong(), x).toInt()
+    }
+}
