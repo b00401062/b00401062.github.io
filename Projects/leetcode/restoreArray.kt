aa4aa9dff4d0f6e1af609a46159fd2c0f10493a5
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-03-21 11:37:51 +0800

    Add files via upload

---
+++ Projects/leetcode/restoreArray.kt
@@ -0,0 +1,32 @@
+package leetcode
+
+import java.util.HashMap
+import leetcode.RestoreArray
+import java.util.ArrayList
+
+object RestoreArray {
+    private fun insertEdge(edges: MutableMap<Int, MutableList<Int>>, v0: Int, v1: Int) {
+        val neighbors = edges.getOrDefault(v0, ArrayList())
+        neighbors.add(v1)
+        edges.putIfAbsent(v0, neighbors)
+    }
+
+    fun restoreArray(adjacentPairs: Array<IntArray>): IntArray {
+        val edges: MutableMap<Int, MutableList<Int>> = HashMap()
+        for (pair in adjacentPairs) {
+            insertEdge(edges, pair[0], pair[1])
+            insertEdge(edges, pair[1], pair[0])
+        }
+        val head = edges.entries.stream()
+            .filter { (_, value): Map.Entry<Int, List<Int>> -> value.size == 1 }
+            .findFirst().get().key
+        val restoredArray = IntArray(adjacentPairs.size + 1)
+        restoredArray[0] = head
+        for (i in 1 until restoredArray.size) {
+            val neighbors: List<Int> = edges[restoredArray[i - 1]]!!
+            restoredArray[i] =
+                if (neighbors.size == 1) neighbors[0] else neighbors[0] xor neighbors[1] xor restoredArray[i - 2]
+        }
+        return restoredArray
+    }
+}
