f06dbb0c05628c5b17aa5925d4ecc1934e8bfa14
Wen-Bin Luo <b00401062@ntu.edu.tw>
2022-03-21 11:29:50 +0800

    Add files via upload

---
+++ Projects/leetcode/find132pattern.kt
@@ -0,0 +1,24 @@
+package leetcode
+
+import java.util.Stack
+
+fun find132pattern(nums: IntArray): Boolean {
+    val n = nums.size
+    val stack = Stack<Range>()
+    stack.push(Range(Int.MAX_VALUE, Int.MAX_VALUE))
+    for (num in nums) {
+        val min = Math.min(num, stack.peek().min)
+        while (stack.peek().max <= num) {
+            stack.pop()
+        }
+        val range = stack.peek()
+        if (num <= range.min) {
+            stack.push(Range(min, num))
+        } else if (range.min < num && num < range.max) {
+            return true
+        }
+    }
+    return false
+}
+
+private class Range internal constructor(val min: Int, val max: Int)
